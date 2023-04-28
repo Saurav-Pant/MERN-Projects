fdf02b9b36421bf22c45f5bac2deb98c5a5ad244
Saurav Pant <pantsaurav005@gmail.com>
2023-04-28 16:23:50 +0530

    Start Todo App

---
+++ 01_Todo_App/api/server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/todo-app", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3001");
+});
