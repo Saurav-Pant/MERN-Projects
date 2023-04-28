40b6dd3ff2353e5926dccd20b767ca2320bb7c9c
Saurav Pant <pantsaurav005@gmail.com>
2023-04-28 21:19:34 +0530

    Backend Done

--- 01_Todo_App/api/server.js
+++ 01_Todo_App/api/server.js
@@ -19,6 +19,33 @@ mongoose
     console.log(err);
   });
 
+const Todo = require("./models/Todo");
+
+app.get("/todos", async (req, res) => {
+  const todos = await Todo.find();
+  res.json(todos);
+});
+
+app.post("/todo/new", (req, res) => {
+  const todo = new Todo({
+    text: req.body.text,
+  });
+  todo.save();
+  res.json(todo);
+});
+
+app.delete("/todos/delete/:id", async (req, res) => {
+  const result = await Todo.findByIdAndDelete(req.params.id);
+  res.json(result);
+});
+
+app.put("/todos/complete/:id", async (req, res) => {
+  const result = await Todo.findByIdAndUpdate(req.params.id, {
+    complete: true,
+  });
+  res.json(result);
+});
+
 app.listen(3000, () => {
-  console.log("Server is running on port 3001");
+  console.log("Server is running on port 3000");
 });
