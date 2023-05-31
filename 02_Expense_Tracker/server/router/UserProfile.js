2b2d50193450869cb7f29c55a4e6cb441e49b03d
Saurav Pant <pantsaurav005@gmail.com>
2023-05-31 12:06:18 +0530

    Routes Added

---
+++ 02_Expense_Tracker/server/router/UserProfile.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const router = express.Router();
+
+const UserProfile = require("../models/UserProfile");
+
+router.post("/profile", async (req, res) => {
+  try {
+    const { image, name } = req.body;
+    const newProfile = new UserProfile({
+      image,
+      name,
+    });
+    await newProfile.save();
+    res.status(201).json(newProfile);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to save the profile." });
+  }
+});
+
+router.get("/profile", async (req, res) => {
+  try {
+    const profile = await UserProfile.find();
+    res.status(200).json(profile);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to fetch profile." });
+  }
+});
+
+module.exports = router;
