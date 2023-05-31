3c2b2aff9f8cabdd182f82a347eb323d989d06e5
Saurav Pant <pantsaurav005@gmail.com>
2023-05-31 10:59:35 +0530

    profile created

--- 02_Expense_Tracker/Client/src/components/profile.jsx
+++ 02_Expense_Tracker/Client/src/components/profile.jsx
@@ -1,11 +1,78 @@
-import React from 'react'
+import React, { useState } from "react";
 
-const profile = () => {
+const Profile = () => {
+  const [image, setImage] = useState(null);
+  const [name, setName] = useState("");
+
+  const handleImageChange = (e) => {
+    const selectedImage = e.target.files[0];
+    setImage(URL.createObjectURL(selectedImage));
+  };
+
+  const handleNameChange = (e) => {
+    setName(e.target.value);
+  };
+
+  const handleImageClick = () => {
+    document.getElementById("imageUpload").click();
+  };
+
+  const handleSave = (e) => {
+    e.preventDefault();
+    // Perform save logic with the selected image and name
+    console.log("Saved:", image, name);
+  };
+
   return (
-    <div>
-        
-    </div>
-  )
-}
+    <div className="container mx-auto h-[80vh]">
+      <form onSubmit={handleSave}>
+        <div className="mt-8 flex items-center justify-center">
+          <div
+            className="w-32 h-32 rounded-full overflow-hidden cursor-pointer"
+            onClick={handleImageClick}
+          >
+            {image ? (
+              <img
+                className="w-full h-full object-cover"
+                src={image}
+                alt="Profile"
+              />
+            ) : (
+              <div className="bg-gray-200 flex items-center justify-center w-full h-full">
+                <span className="text-gray-400 justify-center items-center text-center">
+                  Select Image
+                </span>
+              </div>
+            )}
+          </div>
 
-export default profile
+          <div className="ml-4">
+            <input
+              type="file"
+              id="imageUpload"
+              className="hidden"
+              accept="image/*"
+              onChange={handleImageChange}
+            />
+            <input
+              type="text"
+              className="mt-2 border border-gray-300 px-4 py-2 rounded"
+              placeholder="Enter your name"
+              value={name}
+              onChange={handleNameChange}
+            />
+          </div>
+        </div>
+
+        <button
+          type="submit"
+          className="bg-green-500 hover:bg-green-600 text-white px-4 py-2 mt-4 rounded flex "
+        >
+          Save
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Profile;
