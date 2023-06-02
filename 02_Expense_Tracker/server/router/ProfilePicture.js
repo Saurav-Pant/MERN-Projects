73c053867e35ebbd92dc4371ab5013800e86ac8d
Saurav Pant <pantsaurav005@gmail.com>
2023-06-02 20:11:17 +0530

    debugging

--- 02_Expense_Tracker/server/router/ProfilePicture.js
+++ 02_Expense_Tracker/server/router/ProfilePicture.js
@@ -7,6 +7,7 @@ router.get("/profile/image", async (req, res) => {
   try {
     const user = await User.findById(req.user.id).select("profile");
     res.json({ profile: user.profile });
+    console.log(user.profile);
   } catch (err) {
     console.error(err.message);
     res.status(500).send("Server Error");
