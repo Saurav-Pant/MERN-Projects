4dc9002e5b977a81e40a3ce29e20615cbb89d1a7
Saurav Pant <pantsaurav005@gmail.com>
2023-06-08 15:21:02 +0530

    github button added to home Page

--- 02_Expense_Tracker/Client/src/components/Navbar.jsx
+++ 02_Expense_Tracker/Client/src/components/Navbar.jsx
@@ -4,13 +4,14 @@ import { ThemeContext } from "../context/theme";
 import { motion } from "framer-motion";
 import Profile from "./ProfileIcon";
 import { useLocation } from "react-router-dom";
+import { FiGithub } from "react-icons/fi";
 
 const Header = () => {
   const { theme, toggleTheme } = useContext(ThemeContext);
   const location = useLocation();
   return (
     <>
-      <div className="border-b-2 border-blue-500 transition-all hover:border-blue-700">
+      <div className="border-b-2 border-blue-500 transition-all hover:border-blue-600">
         <div className="max-w-screen-xl mx-auto px-6 py-4 flex justify-between items-center">
           <div className="flex items-center">
             <Link to="/">
@@ -27,6 +28,17 @@ const Header = () => {
             </Link>
           </div>
           <div className="flex justify-center items-center">
+          <div className="mr-5 mt-2 hover:text-blue-500">
+              <motion.button
+                whileHover={{ scale: 1.1 }}
+                whileTap={{ scale: 0.9 }}
+                className="text-3xl drop-shadow-2xl"
+                onClick={toggleTheme}
+              >
+                <FiGithub/>
+                
+              </motion.button>
+            </div>
             <div>
               <motion.button
                 whileHover={{ scale: 1.1 }}
