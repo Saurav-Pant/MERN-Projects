79286c945e9ed30b4c9bb63202d09ee7c65c2e23
Saurav Pant <pantsaurav005@gmail.com>
2023-06-08 20:34:34 +0530

    blank stuff added to footer links

--- 02_Expense_Tracker/Client/src/components/Footer.jsx
+++ 02_Expense_Tracker/Client/src/components/Footer.jsx
@@ -5,15 +5,17 @@ import { Link } from "react-router-dom";
 const Footer = () => {
   return (
     <div className="flex justify-center items-center border-t-2 border-blue-300 py-4">
-      <p className="text-gray-500 text-xl">&copy; 2023 Expense Tracker</p>
+      <p className="text-gray-500 text-xl hover:text-blue-400 transition-colors ">
+        &copy; 2023 Expense Tracker
+      </p>
       <div className="flex ml-4 text-2xl">
         <div className=" text-gray-500 hover:text-blue-500 transition-colors duration-300 mx-2 ">
-          <Link to="https://twitter.com">
+          <Link to="https://twitter.com" target="_blank">
             <FiTwitter />
           </Link>
         </div>
         <div className=" text-gray-500 hover:text-blue-500 transition-colors duration-300 mx-2">
-          <Link to="https://www.linkedin.com/feed/">
+          <Link to="https://www.linkedin.com/feed/" target="_blank">
             <FiLinkedin />
           </Link>
         </div>
