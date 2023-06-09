55558ba0e61e53fc059324ead8658edfd79d3da7
Saurav Pant <pantsaurav005@gmail.com>
2023-06-09 09:00:21 +0530

    prev Img removed

--- 02_Expense_Tracker/Client/src/components/Header.jsx
+++ 02_Expense_Tracker/Client/src/components/Header.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { motion } from 'framer-motion';
-import last from "../asset/last.png";
 import Person from "../asset/Person.png";
 import { Link } from "react-router-dom";
 
