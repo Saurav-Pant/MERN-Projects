41d2007e6fa5edcb2ad07c57342c0f57006cdc50
Saurav Pant <pantsaurav005@gmail.com>
2023-06-09 09:29:41 +0530

    Dashboard Hover well

--- 02_Expense_Tracker/Client/src/components/Dashboard.jsx
+++ 02_Expense_Tracker/Client/src/components/Dashboard.jsx
@@ -161,30 +161,23 @@ const Dashboard = () => {
               >
                 <motion.div
                   key={record._id}
-                  className="rounded-lg shadow-md p-4 mb-4 mr-4 relative w-56 h-40 bg-gradient-to-r from-cyan-500 to-cyan-400 dark:from-blue-700 dark:to-blue-500 transform transition duration-300 ease-in-out"
-                  whileTap={{ scale: 0.95 }}
-                  whileHover={{
-                    boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.25)",
-                  }}
-                  style={{
-                    perspective: "500px",
-                    transformStyle: "preserve-3d",
-                  }}
+                  className="rounded-lg shadow-xl p-4 mb-4 mr-4 relative w-56 h-40 bg-gradient-to-r from-purple-500 to-indigo-500 transform transition-all duration-300 hover:shadow-2xl  hover:rotate-1 hover:bg-gradient-to-r hover:from-indigo-500 hover:to-blue-500"
                 >
-                  <div className="hover:bg-opacity-50 hover:bg-gray-200 rounded-lg p-4 w-full h-full absolute top-0 left-0 backdrop-filter backdrop-blur-md transition duration-300 ease-in-out"></div>
-                  <p className="text-lg font-semibold mb-2 text-center text-white z-10 relative">
-                    {record.title}
-                  </p>
-                  <p className="text-gray-300 mb-2 absolute bottom-7 right-2 text-2xl flex justify-center items-center z-10 ">
-                    <span className="text-green-300 mr-1">&#x20B9;</span>
-                    {record.amount}
-                  </p>
-                  <p className="text-gray-300 mb-2 text-center z-10 relative">
-                    {formatDate(record.date)}
-                  </p>
-                  <p className="text-gray-300 mt-10 z-10 relative">
-                    {record.description}
-                  </p>
+                  <div className="rounded-lg p-4 w-full h-full absolute top-0 left-0 backdrop-filter backdrop-blur-md transition duration-300 ease-in-out">
+                    <p className="text-lg font-semibold mb-2 text-center text-white z-10 relative">
+                      {record.title}
+                    </p>
+                    <p className="text-gray-300 mb-2 absolute bottom-7 right-2 text-2xl flex justify-center items-center z-10 ">
+                      <span className="text-green-300 mr-1">&#x20B9;</span>
+                      {record.amount}
+                    </p>
+                    <p className="text-gray-300 mb-2 text-center z-10 relative">
+                      {formatDate(record.date)}
+                    </p>
+                    <p className="text-gray-300 mt-10 z-10 relative">
+                      {record.description}
+                    </p>
+                  </div>
                 </motion.div>
               </Link>
             ))
