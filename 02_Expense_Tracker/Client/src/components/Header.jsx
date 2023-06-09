d8026828527a9dcec618a4b1aa0a8098436b76e4
Saurav Pant <pantsaurav005@gmail.com>
2023-06-09 08:59:57 +0530

    Header Done

--- 02_Expense_Tracker/Client/src/components/Header.jsx
+++ 02_Expense_Tracker/Client/src/components/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
       <div className="w-full max-w-[1536px]  px-4 md:px-8 lg:px-20">
         <div className="mt-16 md:mt-0 md:h-screen 2xl:h-auto py-16 2xl:py-28 grid md:grid-cols-2 items-center">
           <div className="flex flex-col items-start gap-10">
-            <h1 className="text-5xl md:text-[6vw] lg:text-8xl font-black">
+            <h1 className="text-6xl lg:text-8xl font-black text-blue-400 hover:text-blue-500 transition-colors ">
               Expense Tracker
             </h1>
             <p className="text-base sm:text-base md:text-2xl opacity-60 dark:opacity-80 mb-2 md:mb-4">
