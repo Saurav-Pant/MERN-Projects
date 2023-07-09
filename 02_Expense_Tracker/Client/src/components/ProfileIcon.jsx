0e70b9ea95b01521f4f97da62d538c379131b091
Saurav Pant <pantsaurav005@gmail.com>
2023-07-09 16:20:23 +0530

    Loading UPdated

--- 02_Expense_Tracker/Client/src/components/ProfileIcon.jsx
+++ 02_Expense_Tracker/Client/src/components/ProfileIcon.jsx
@@ -3,15 +3,21 @@ import { CgProfile } from "react-icons/cg";
 import { motion } from "framer-motion";
 import { AuthContext } from "../context/AuthContext";
 import { useNavigate } from "react-router-dom";
+import LoadingSpinner from "./Loading";
 
 const ProfileIcon = () => {
   const { logout } = useContext(AuthContext);
   const [showLogout, setShowLogout] = useState(false);
+  const [loggingOut, setLoggingOut] = useState(false);
   const navigate = useNavigate();
 
   const handleLogout = () => {
+    setLoggingOut(true);
     logout();
-    navigate("/");
+    setTimeout(() => {
+      navigate("/");
+      setLoggingOut(false);
+    }, 2000);
   };
 
   const toggleLogout = () => {
@@ -26,10 +32,10 @@ const ProfileIcon = () => {
         className="p-2 focus:outline-none"
         onClick={toggleLogout}
       >
-        <CgProfile size={50} className="pl-5"/>
+        <CgProfile size={50} className="pl-5" />
       </motion.button>
 
-      {showLogout && (
+      {showLogout && !loggingOut && (
         <div className="ml-2">
           <button
             className="px-4 py-2 pl-5 rounded bg-red-500 hover:bg-red-700 text-white font-bold"
@@ -37,6 +43,12 @@ const ProfileIcon = () => {
           >
             Logout
           </button>
+        </div>
+      )}
+
+      {loggingOut && (
+        <div className="ml-2">
+          <LoadingSpinner />
         </div>
       )}
     </div>
