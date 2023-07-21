d0f2cc3f6e4ad0e73f29a931b149ca108c1e9e51
Saurav Pant <pantsaurav005@gmail.com>
2023-07-21 15:59:45 +0530

    Code Refactored

--- 02_Expense_Tracker/server/server.js
+++ 02_Expense_Tracker/server/server.js
@@ -1,63 +1,12 @@
+// app.js (or your main file name)
+
 require("dotenv").config();
-const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
-const session = require("express-session");
-const passport = require("passport");
-// require("./passport");
-const expenseRouter = require("./router/routers");
-const createRoute = require("./router/createRoute");
-const editRoute = require("./router/editRoute");
-const SignupRoute = require("./router/SignUp");
-const LoginRoute = require("./router/Login");
+const app = require("./index");
+const db = require("./utils/db"); // Connect to MongoDB
 
-const app = express();
-const MONGO_URI = process.env.LOCAL_MONGO_URL;
-
-app.use(express.json({ limit: "10mb" }));
-
-app.use(
-  session({
-    secret: "sauravpant123",
-    resave: false,
-    saveUninitialized: false,
-  })
-);
-
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    credentials: true,
-  })
-);
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-app.use("/auth", expenseRouter);
-
-app.use("/api/records", createRoute); // Route for expense records
-
-app.use("/edit", editRoute); // Route for edit
-
-app.use("/SignUp", SignupRoute); // Route for Signup
-
-app.use("/Login", LoginRoute); // Route for Login
-
-mongoose
-  .connect(MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log("Connected to MongoDB");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+const PORT = 3001;
 
 // Start the server
-app.listen(3001, () => {
-  console.log("Server is running...");
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}...`);
 });
