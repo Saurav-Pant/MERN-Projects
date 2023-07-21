9fbf90618d6a25f5527da3cdd92c643296bce765
Saurav Pant <pantsaurav005@gmail.com>
2023-07-21 16:03:07 +0530

    code refactored

--- 02_Expense_Tracker/server/server.js
+++ 02_Expense_Tracker/server/server.js
@@ -1,12 +1,9 @@
-// app.js (or your main file name)
-
 require("dotenv").config();
 const app = require("./index");
 const db = require("./utils/db"); // Connect to MongoDB
 
 const PORT = 3001;
 
-// Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}...`);
 });
